c7cd7bc80763702845298c5fff3db4d6c4c0758b
Narenselvam <126243407+Narenselvam@users.noreply.github.com>
2024-10-04 22:30:13 +0530

    sub-class

---
+++ src/bookingSystem/TicketBookingSystem.java
@@ -0,0 +1,15 @@
+package bookingSystem;
+
+public class TicketBookingSystem {
+
+    private Event event;
+
+
+    public Event create_event(String event_name,String date,String time,
+                              int total_seats,double ticket_price){
+
+
+
+        return event;
+    }
+}
